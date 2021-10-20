ec3438102ad909364833dc585ee4b61fa46e08cf
MisganaDaniel <danielmigun@gmail.com>
2021-10-20 15:37:45 +0300

    post

--- main.js
+++ main.js
@@ -28,3 +28,22 @@ function feedback(){
     }; 
     client.send(requestBody);
 }
+function feed() {
+    var requestBody = "{\"feedback\":\"Nice\",\"first_name\":\"Misgun\"}"; 
+
+    var client=new XMLHttpRequest();
+    client.open("post","https://dev105946.service-now.com/api/now/table/x_677549_dengene_0_feedback");
+
+    client.setRequestHeader('Accept','application/json');
+    client.setRequestHeader('Content-Type','application/json');
+
+    //Eg. UserName="admin", Password="admin" for this code sample.
+    client.setRequestHeader('Authorization', 'Basic '+btoa('admin'+':'+'Al4i2lCSAziO'));
+
+    client.onreadystatechange = function() { 
+        if(this.readyState == this.DONE) {
+            document.getElementById("response").innerHTML=this.status + this.response; 
+        }
+    }; 
+    client.send(requestBody);
+}
