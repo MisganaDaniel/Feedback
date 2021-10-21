41b1b32587b23764f9df3d99c163e043c34b05ee
MisganaDaniel <danielmigun@gmail.com>
2021-10-21 11:42:38 +0300

    form

--- index.php
+++ index.php
@@ -58,15 +58,15 @@
     </div>
     <script>
         function feedback(){
-            //var requestBody = "{\"first_name\":\"Misgana\",\"last_name\":\"Daniel\",\"email\":\"danielmisgun@gmail.com\",\"date\":\"\",\"feedback\":\"Perfect\"}"; 
+            var requestBody = "{\"first_name\":\"Misgana\",\"last_name\":\"Daniel\",\"email\":\"danielmisgun@gmail.com\",\"date\":\"\",\"feedback\":\"Perfect\"}"; 
 
-            var firstName = document.getElementById("firstName");
-            var lastName = document.getElementById("lastName");
-            var email = document.getElementById("email");
-            var date = document.getElementById("Date");
-            var feedback = document.getElementById("feedback");
+            // var firstName = document.getElementById("firstName");
+            // var lastName = document.getElementById("lastName");
+            // var email = document.getElementById("email");
+            // var date = document.getElementById("Date");
+            // var feedback = document.getElementById("feedback");
 
-            var requestBody = "{\"first_name\":\"" + firstName + "\",\"last_name\":\"" + lastName + "\",\"email\":\"" + email + "\",\"date\":\"" + date + "\",\"feedback\":\"" + feedback + "\"}";
+            // var requestBody = "{\"first_name\":\"" + firstName + "\",\"last_name\":\"" + lastName + "\",\"email\":\"" + email + "\",\"date\":\"" + date + "\",\"feedback\":\"" + feedback + "\"}";
             
             var client=new XMLHttpRequest();
             client.open("post","https://dev105946.service-now.com/api/now/table/x_677549_dengene_0_feedback");
