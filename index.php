335b181785192b4419c331a770b7a3c5b3988ccd
MisganaDaniel <danielmigun@gmail.com>
2021-10-21 14:08:22 +0300

    jquery

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="main.js"></script>
-    
+    <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
     <title>Feedback</title>
   </head>
   <body>
