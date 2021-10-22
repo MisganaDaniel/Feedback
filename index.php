02af1c10b561c0b9bce924e18b07ae6bbbf63503
MisganaDaniel <danielmigun@gmail.com>
2021-10-22 12:02:07 +0300

    ll

--- index.php
+++ index.php
@@ -86,13 +86,19 @@
             client.send(requestBody);
             
         }
-    </script>
-   <script>
-        //var buttonSubmit = document.getElementById("submit").addEventListener("click",feedback);
+
         var buttonSubmit = document.getElementById("submit");
         buttonSubmit.addEventListener('click', function(event){
             //console.log('Button Clicked');
             feedback();
+        });
+    </script>
+   <script>
+        //var buttonSubmit = document.getElementById("submit").addEventListener("click",feedback);
+        // var buttonSubmit = document.getElementById("submit");
+        // buttonSubmit.addEventListener('click', function(event){
+        //     //console.log('Button Clicked');
+        //     feedback();
         });
         //buttonSubmit.addEventListener("click",feedback);
    </script>
