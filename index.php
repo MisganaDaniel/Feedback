9643ea2500cc3e76ec71852012d3b5ceffa9ab58
MisganaDaniel <danielmisgun@gmail.com>
2021-10-23 17:18:45 +0300

    helooooo

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="main.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
-    <title>Feedback</title>
+    <title>Feedbacks</title>
   </head>
   <body>
     <div class="card shadow-lg" style="width: 1000px;margin-left:150px;margin-top:100px;border-radius:12px;">
